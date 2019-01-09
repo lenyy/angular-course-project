03c8a605b3c2c20a422ebc6cac229f826e47e9de
lenyy <pedro.rosa90@gmail.com>
2019-01-09 16:32:11 +0000

    Routing Done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,4 +34,8 @@ export class RecipeService {
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
   }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
 }
